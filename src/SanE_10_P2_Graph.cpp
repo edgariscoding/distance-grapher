644b9b4a0aa76204d13f7490d9d2852e23b15c1b
Edgar <edgr.sanchez@outlook.com>
2015-10-06 11:51:20 -0400

    New folder structure

--- src/SanE_10_P2_Graph.cpp
+++ src/SanE_10_P2_Graph.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
+#define MAXNODES 26
+
 int main ()
 {
 	cout << "\nThis is P2: Graph\n\n";
+	
+	while(play)
+	{
+		std::string Line; // string to read line in
+		std::cout << "cmd>";
+		std::getline(std::cin, Line); // read line from screen
+		if(Line.empty())
+		{
+			std::cout << "Didn't input anything try again!";
+			std::cout << std::endl;
+			std::cin.clear();
+			continue;
+		}
+		std::vector<std::string> Parsed; // vector to store parsed words
+		LineParser(Line, Parsed, false); // function to parse words into vector
 	return 0;
 }
